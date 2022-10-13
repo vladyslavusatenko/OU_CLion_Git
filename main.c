4b851ce5eebadd46990b6939e8bec4b56654d979
vladyslavusatenko <245127@edu.p.lodz.pl>
2022-10-13 20:42:10 +0200

    nowa metoda policz

--- main.c
+++ main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
+
+
+int Policz(int a, int b) {
+    return a + b;
+}
+
 int main() {
-    printf("Hello, World!\n");
+    printf("Hello, World!");
+    printf("W pierwszym branchu; 2+3= %d",  Policz(2, 3));
     return 0;
 }
